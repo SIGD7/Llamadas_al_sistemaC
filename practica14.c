25c726add5bdf3c97fe04a515817559d4db4603e
SIGD7 <diavigaser.5.4.7@gmail.com>
2017-01-27 01:42:50 -0600

    Aleatorio

---
+++ practica14.c
@@ -0,0 +1,24 @@
+/*Sergio Iván Galindo Díaz
+Práctica #14
+
+*/
+
+#include <fcntl.h>      
+#include <stdlib.h>
+#include <stdio.h>
+void main ()
+{
+   int cadenaLeida;
+
+   int fichero = open ("/dev/random", O_RDONLY);
+
+   if (fichero==-1)
+   {
+        perror("Error al abrir fichero:");
+        exit(1);
+   }
+    read(fichero, &cadenaLeida, sizeof(int));
+   close(fichero);
+
+   printf ( "Total leido %d\n",&cadenaLeida);
+}
